a34eb1e7da7b03abbd74ee90e24333725379ec7f
ShreyasDhale <shreyasdhale7249@gmail.com>
2024-07-12 17:37:01 +0530

    new Commit

--- index.js
+++ index.js
@@ -8,6 +8,21 @@ const PORT = process.env.PORT || 3000;
 const server = app.listen(PORT, () => { console.log('Server Started at PORT:', PORT) })
 
 const io = require('socket.io')(server)
+
+const connectedUser = new Set();
 io.on('connection', (socket) => {
     console.log("connected successfully", socket.id)
+    connectedUser.add(socket.id);
+    io.emit('connected-user', connectedUser.size)
+
+    socket.on('disconnect', () => {
+        console.log("Disconnected ", socket.id)
+        connectedUser.delete(socket.id);
+        io.emit('connected-user', connectedUser.size)
+    })
+
+    socket.on('message', (data) => {
+        console.log(data)
+        socket.broadcast.emit('message-receive', data);
+    })
 })
